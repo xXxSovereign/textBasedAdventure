49e60ce714e0bf2ad5bba7c2168248bf13707db6
xXxSovereign <michael.w.adolph@gmail.com>
2022-02-11 10:31:23 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player user = new Player(10, 5);
+
+        System.out.println(user.hp);
+    }
+
+
+
+    // start
+
+
+
+
+
+}
---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+
+    int hp, weaponDmg, balance;
+
+    public Player(int hp, int dmg){
+
+        this.hp = hp;
+        this.weaponDmg = dmg;
+    }
+
+}

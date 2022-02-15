60a6c8c1f042386328afac4e2743cffee22caa81
xXxSovereign <michael.w.adolph@gmail.com>
2022-02-15 10:06:48 -0500

    setup main

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-
+    static char choice;
 
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("You wake up in a dilapidated building, with an eerie silence... ");
+        System.out.println("""
+                Who are you?\s
+                1. I am an ecologist
+                2. I am a loner
+                3. I am a mercenary""");
 
-
+        choice = scan.nextLine().charAt(0);
 
     }
 
